03e2c1d3bfa75b857db1c30ef2c1094ed184c8ed
vboxuser <tatyana.sharamet@gmail.com>
2025-03-16 19:09:46 +0000

    Add hr hospital

---
+++ task_2/hr_hospital/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Hr Hospital',
+    'summary': '',
+    'author': 'Tetiana Sharamet',
+    'category': 'Customizations',
+    'license': 'OPL-1',
+    'version': '17.0.0.0.1',
+
+    'depends': ['base'],
+
+    'external_dependencies': {'python': []},
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_hospital_menu.xml',
+        'views/hr_hospital_patient_patient_views.xml',],
+
+
+    'installable': True,
+    'auto_install': False,
+}
