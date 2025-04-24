e1c8788656e89fae51dd40aab4eb08ec10b6b3cf
vboxuser <tatyana.sharamet@gmail.com>
2025-04-24 06:39:11 +0000

    Add  t6

--- hr_hospital/models/__init__.py
+++ hr_hospital/models/__init__.py
@@ -4,5 +4,4 @@ from . import (hr_hospital_person,
                hr_hospital_disease,
                hr_hospital_visit,
                hr_hospital_diagnosis,
-               res_users,
                )
